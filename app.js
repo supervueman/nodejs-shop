50cea6d767e059946e412e9d1353ac71ec1676ef
supervueman <supervueman@gmail.com>
2019-05-21 10:05:17 +0400

    Add pug and set settings to app.js for pug

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ const shopRoutes = require('./routes/shop');
 
 const app = express();
 
+app.set('view engine', 'pug');
+app.set('views', 'views');
+
 app.use(
 	bosyParser.urlencoded({
 		extended: false
@@ -20,4 +23,6 @@ app.use((req, res, next) => {
 	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
-app.listen(3000, () => {});
+app.listen(3000, () => {
+	console.log('App started on 3000 port');
+});
