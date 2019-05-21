bb91642d04acd606eba4c0a4dd7dcb659d3e4c6f
supervueman <supervueman@gmail.com>
2019-05-21 09:32:54 +0400

    Start node shop

---
+++ app.js
@@ -0,0 +1,23 @@
+const path = require('path');
+const express = require('express');
+const bosyParser = require('body-parser');
+
+const adminData = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
+
+const app = express();
+
+app.use(
+	bosyParser.urlencoded({
+		extended: false
+	})
+);
+app.use(express.static(path.join(__dirname, 'public')));
+app.use('/admin', adminData.routes);
+app.use(shopRoutes);
+
+app.use((req, res, next) => {
+	res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+});
+
+app.listen(3000, () => {});
---
+++ routes/shop.js
@@ -0,0 +1,13 @@
+const path = require('path');
+const express = require('express');
+
+const rootDir = require('../util/path');
+const adminData = require('./admin');
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+	console.log(adminData);
+	res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+});
+
+module.exports = router;
