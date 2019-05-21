39044bbaa02819eb75058dc1d2bdfb728c8eb417
supervueman <supervueman@gmail.com>
2019-05-21 23:41:51 +0400

    Add ejs

--- app.js
+++ app.js
@@ -1,19 +1,13 @@
 const path = require('path');
 const express = require('express');
 const bosyParser = require('body-parser');
-const expressHbs = require('express-handlebars');
 
 const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const app = express();
 
-app.engine('hbs', expressHbs({
-	layoutsDir: 'views/layouts/',
-	defaultLayout: 'main-layout',
-	extname: 'hbs'
-}));
-app.set('view engine', 'hbs');
+app.set('view engine', 'ejs');
 app.set('views', 'views');
 
 app.use(
