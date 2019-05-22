6cba2598e9238d62a9c9dfb90d9cc0f3631639a1
supervueman <supervueman@gmail.com>
2019-05-22 10:15:52 +0400

    Add error controller

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const path = require('path');
 const express = require('express');
 const bosyParser = require('body-parser');
 
+const errorController = require('./controllers/error');
+
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
@@ -15,15 +17,12 @@ app.use(
 		extended: false
 	})
 );
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use('/', shopRoutes);
 
-app.use((req, res, next) => {
-	res.status(404).render('404', {
-		pageTitle: 'Page not found'
-	});
-});
+app.use(errorController.get404);
 
 app.listen(3000, () => {
 	console.log('App started on 3000 port');
