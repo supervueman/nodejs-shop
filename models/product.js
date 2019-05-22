ee447ba0477a03f8ea759769c43465b072eae5c1
supervueman <supervueman@gmail.com>
2019-05-22 22:43:26 +0400

    New project

--- models/product.js
+++ models/product.js
@@ -7,18 +7,22 @@ const p = path.join(
   'products.json'
 );
 
-const getProductsFromFile = (cb) => {
+const getProductsFromFile = cb => {
   fs.readFile(p, (err, fileContent) => {
     if (err) {
       cb([]);
     } else {
       cb(JSON.parse(fileContent));
     }
-  })
-}
+  });
+};
+
 module.exports = class Product {
-  constructor(t) {
-    this.title = t;
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
