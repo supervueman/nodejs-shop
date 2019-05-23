b7efdecde02d6755ff03dd67d767a37897c2108c
supervueman <supervueman@gmail.com>
2019-05-23 10:01:50 +0400

    Add form for add cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -12,8 +12,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.getProduct = (req, res, next) => {
   const prodId = req.params.productId;
-  console.log(prodId);
-  res.redirect('/')
+  Product.findById(prodId, product => {
+    res.render('shop/product-detail', {
+      product,
+      pageTitle: product.title,
+      path: '/products'
+    })
+  });
 };
 
 exports.getIndex = (req, res, next) => {
@@ -31,6 +36,12 @@ exports.getCart = (req, res, next) => {
     path: '/cart',
     pageTitle: 'Your Cart'
   });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  console.log(prodId);
+  res.redirect('/cart');
 };
 
 exports.getOrders = (req, res, next) => {
