9636c649834eb513750d56fe5aa66da0337bb896
supervueman <supervueman@gmail.com>
2019-05-23 11:27:52 +0400

    Start update product

--- controllers/admin.js
+++ controllers/admin.js
@@ -39,6 +39,10 @@ exports.getEditProduct = (req, res, next) => {
   });
 };
 
+exports.postEditProduct = (req, res, next) {
+
+};
+
 exports.getProducts = (req, res, next) => {
   Product.fetchAll(products => {
     res.render('admin/products', {
