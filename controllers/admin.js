b123cfd16305a70e24c2d96088d94a15aa4da8ef
supervueman <supervueman@gmail.com>
2019-05-23 11:22:14 +0400

    Edit product view

--- controllers/admin.js
+++ controllers/admin.js
@@ -4,6 +4,7 @@ exports.getAddProduct = (req, res, next) => {
   res.render('admin/edit-product', {
     pageTitle: 'Add Product',
     path: '/admin/add-product',
+    editing: false
   });
 };
 
@@ -19,15 +20,22 @@ exports.postAddProduct = (req, res, next) => {
 
 exports.getEditProduct = (req, res, next) => {
   const editMode = req.query.edit;
-
   if (!editMode) {
     return res.redirect('/');
   }
 
-  res.render('admin/edit-product', {
-    pageTitle: 'Edit Product',
-    path: '/admin/edit-product',
-    editing: editMode
+  const prodId = req.params.productId;
+
+  Product.findById(prodId, product => {
+    if (!product) {
+      return res.riderect('/')
+    }
+    res.render('admin/edit-product', {
+      pageTitle: 'Edit Product',
+      path: '/admin/edit-product',
+      editing: editMode,
+      product
+    });
   });
 };
 
