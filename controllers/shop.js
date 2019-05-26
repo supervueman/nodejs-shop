c86a89007d5f43b724ac4254fdbceab04e6b0014
supervueman <supervueman@gmail.com>
2019-05-26 22:28:19 +0400

    Add method for get all products

--- controllers/shop.js
+++ controllers/shop.js
@@ -2,14 +2,14 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll().then(([rows, fieldData]) => {
+  Product.findAll().then(products => {
     res.render('shop/product-list', {
-      prods: rows,
-      pageTitle: 'All Products',
+      prods: products,
+      pageTitle: 'Shop',
       path: '/products'
     });
   }).catch(err => {
-    console.log(err);
+    console.log(err)
   });
 };
 
@@ -27,16 +27,15 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll().then(([rows, fieldData]) => {
+  Product.findAll().then(products => {
     res.render('shop/index', {
-      prods: rows,
+      prods: products,
       pageTitle: 'Shop',
       path: '/'
     });
   }).catch(err => {
-    console.log(err);
+    console.log(err)
   });
-
 };
 
 exports.getCart = (req, res, next) => {
