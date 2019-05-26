2a5fc9f70754df2a89496cec3cff017024ca017d
supervueman <supervueman@gmail.com>
2019-05-26 21:01:28 +0400

    Add fetchAllProducts method

--- app.js
+++ app.js
@@ -24,12 +24,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use('/', shopRoutes);
 
-db.execute('SELECT * FROM products').then(result => {
-	console.log(result[0], result[1])
-}).catch(err => {
-	console.log(err);
-});
-
 app.use(errorController.get404);
 
 app.listen(3000, () => {
--- controllers/shop.js
+++ controllers/shop.js
@@ -2,12 +2,14 @@ const Product = require('../models/product');
 const Cart = require('../models/cart');
 
 exports.getProducts = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll().then(([rows, fieldData]) => {
     res.render('shop/product-list', {
-      prods: products,
+      prods: rows,
       pageTitle: 'All Products',
       path: '/products'
     });
+  }).catch(err => {
+    console.log(err);
   });
 };
 
@@ -23,13 +25,16 @@ exports.getProduct = (req, res, next) => {
 };
 
 exports.getIndex = (req, res, next) => {
-  Product.fetchAll(products => {
+  Product.fetchAll().then(([rows, fieldData]) => {
     res.render('shop/index', {
-      prods: products,
+      prods: rows,
       pageTitle: 'Shop',
       path: '/'
     });
+  }).catch(err => {
+    console.log(err);
   });
+
 };
 
 exports.getCart = (req, res, next) => {
