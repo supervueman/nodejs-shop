e23c9f02ba1b6b387b4679d41e61d3cac9184f34
supervueman <supervueman@gmail.com>
2019-05-26 22:20:53 +0400

    Add method for create product

--- controllers/admin.js
+++ controllers/admin.js
@@ -13,12 +13,17 @@ exports.postAddProduct = (req, res, next) => {
   const imageUrl = req.body.imageUrl;
   const price = req.body.price;
   const description = req.body.description;
-  const product = new Product(null, title, imageUrl, description, price);
-  product.save().then(result => {
-    res.redirect('/');
+  Product.create({
+    title,
+    price,
+    imageUrl,
+    description
+  }).then(result => {
+    // console.log(result);
+    console.log('Created product')
   }).catch(err => {
     console.log(err);
-  });
+  })
 };
 
 exports.getEditProduct = (req, res, next) => {
