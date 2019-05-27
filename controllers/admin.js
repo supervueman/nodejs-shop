ce655ad104b15a99b326bec980b393ce8543b4ec
supervueman <supervueman@gmail.com>
2019-05-27 22:37:34 +0400

    Add user module

--- controllers/admin.js
+++ controllers/admin.js
@@ -22,6 +22,7 @@ exports.postAddProduct = (req, res, next) => {
     .then(result => {
       // console.log(result);
       console.log('Created product');
+      res.redirect('/admin/products');
     })
     .catch(err => {
       console.log(err);
@@ -90,6 +91,13 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.delete(prodId);
-  res.redirect('/admin/products');
+  Product.findByPk(prodId)
+    .then(product => {
+      return product.destroy()
+    })
+    .then(result => {
+      console.log('PRODUCT DELETED');
+      res.redirect('/admin/products');
+    })
+    .catch(err => console.log(err));
 };
