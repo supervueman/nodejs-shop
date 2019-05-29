c99ee2aeb10dac8af6c46d4415c8f2e55601b86b
supervueman <supervueman@gmail.com>
2019-05-29 16:27:52 +0400

    Add method for find one product

--- models/product.js
+++ models/product.js
@@ -1,3 +1,4 @@
+const mongodb = require('mongodb');
 const getDb = require('../util/database').getDb;
 
 class Product {
@@ -21,6 +22,18 @@ class Product {
     }).catch(err => {
       console.log(err)
     });
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db.collection('products').find({
+      _id: new mongodb.ObjectId(prodId)
+    }).next().then(product => {
+      console.log(product);
+      return product;
+    }).catch(err => {
+      console.log(err)
+    })
   }
 }
 
--- routes/shop.js
+++ routes/shop.js
@@ -1,5 +1,3 @@
-const path = require('path');
-
 const express = require('express');
 
 const shopController = require('../controllers/shop');
@@ -10,7 +8,7 @@ router.get('/', shopController.getIndex);
 
 router.get('/products', shopController.getProducts);
 
-// router.get('/products/:productId', shopController.getProduct);
+router.get('/products/:productId', shopController.getProduct);
 
 // router.get('/cart', shopController.getCart);
 
