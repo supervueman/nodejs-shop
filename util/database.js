10ce8890237990d02e3c9a6dc6ac1e0444ac283b
supervueman <supervueman@gmail.com>
2019-05-29 15:03:38 +0400

    Add mongodb connection

--- util/database.js
+++ util/database.js
@@ -1,9 +1,13 @@
-const Sequelize = require('sequelize');
+const mongodb = require('mongodb');
+const MongoClient = mongodb.MongoClient;
 
-const sequelize = new Sequelize('node-complete', 'root', 'password', {
-  dialect: 'mysql',
-  host: 'localhost',
-  port: '33060'
-});
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    `mongodb://${'localhost:27020'}/template`
+  ).then(client => {
+    console.log('Connected');
+    callback(client)
+  }).catch(err => console.log(err));
+}
 
-module.exports = sequelize;
+module.exports = mongoConnect;
