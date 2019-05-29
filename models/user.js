d415c772203c32bc82d7456ba7f2b2c385626e8b
supervueman <supervueman@gmail.com>
2019-05-29 21:45:47 +0400

    View order page

--- models/user.js
+++ models/user.js
@@ -106,9 +106,11 @@ class User {
       });
   }
 
-  getOrder() {
+  getOrders() {
     const db = getDb();
-    return db.collection('orders')
+    return db.collection('orders').find({
+      'user._id': new ObjectId(this._id)
+    }).toArray();
   }
 
   addOrder() {
