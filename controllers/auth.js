37367c9dd46a903fe556d3105f2d4c8ece895f43
supervueman <supervueman@gmail.com>
2019-05-30 14:46:54 +0400

    Add cookie

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,6 +1,13 @@
 exports.getLogin = (req, res, next) => {
+  const isLoggedIn = req.get('Cookie').split(';')[0].trim().split('=')[1] === 'true' ? true : false;
   res.render('auth/login', {
     path: '/login',
-    pageTitle: 'Login'
+    pageTitle: 'Login',
+    isAuthenticated: true
   })
+};
+
+exports.postLogin = (req, res, next) => {
+  res.setHeader('Set-Cookie', 'loggedIn=true; HttpOnly');
+  res.redirect('/');
 };
