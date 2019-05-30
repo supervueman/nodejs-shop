f564a535ee30dc111267a78cfb4af64fa818da62
supervueman <supervueman@gmail.com>
2019-05-30 11:24:53 +0400

    Delete product

--- controllers/admin.js
+++ controllers/admin.js
@@ -93,7 +93,7 @@ exports.getProducts = (req, res, next) => {
 
 exports.postDeleteProduct = (req, res, next) => {
   const prodId = req.body.productId;
-  Product.deleteById(prodId)
+  Product.findByIdAndRemove(prodId)
     .then(() => {
       console.log('PRODUCT DELETED');
       res.redirect('/admin/products');
