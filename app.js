3787001229ea290e81006905943215da0a044018
supervueman <supervueman@gmail.com>
2019-05-30 14:03:06 +0400

    Add login page

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const User = require('./models/user')
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const authRoutes = require('./routes/auth');
 
 const app = express();
 
@@ -33,6 +34,7 @@ app.use((req, res, next) => {
 
 app.use('/admin', adminRoutes);
 app.use('/', shopRoutes);
+app.use('/', authRoutes);
 
 app.use(errorController.get404);
 
